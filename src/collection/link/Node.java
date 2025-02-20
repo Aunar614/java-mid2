095524dadf8ba9cb71664b6b493ed9297f960ab1
Aunar614 <developkim614@gmail.com>
2025-02-20 13:47:05 +0530

    노드와 연결 2

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -8,4 +8,34 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    // IDE 생성 toString()
+/*
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "item=" + item +
+                ", next=" + next +
+                '}';
+    }
+*/
+
+    // 직접 구현 toString()
+    // [A -> B -> C]
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;  // x01 = A
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append(" -> ");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
